c42ea7a1c36ccadef14ed795e027309ebbeaa0a3
Ollie Edgington <ollie_edgington@hotmail.co.uk>
2016-01-26 10:16:27 +0000

    Created clickable contact information which modifies the document.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -2,8 +2,26 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'app',
-    template: 'Hello World!',
+    template: `
+
+        <h3 (click)="onSelect()" [class.clicked]="showDetail === true">
+            {{contact.firstName}} {{contact.lastName}}
+        </h3>
+        <input [(ngModel)]="contact.firstName" type="text">
+        <div *ngIf="showDetail === true">
+            Phone Number: {{contact.phone}}<br>
+            Email: {{contact.email}}
+        </div>
+    `,
+    styleUrls: ["../src/css/app.css"]
 })
 export class AppComponent {
 
+    public contact = {firstName:'Ollie', lastName:'Edgington', phone:'07583197423', email:'ollie_edgington@hotmail.co.uk'};
+
+    public showDetail = false;
+
+    onSelect(){
+        this.showDetail = true;
+    }
 }
